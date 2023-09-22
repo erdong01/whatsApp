55917f2d34d28327a84a7bfa500d8359949fa1e7
erdong01 <88155745@qq.com>
2023-09-22 22:15:38 +0800

    no message

--- initialize/Routers.go
+++ initialize/Routers.go
@@ -10,5 +10,8 @@ func Routers() *gin.Engine {
 	Router := gin.Default()
 	userGroup := Router.Group("user")
 	userGroup.GET("/whats_login", user.Login)
+
+	msgGroup := Router.Group("msg")
+	msgGroup.POST("/send", user.Login)
 	return Router
 }
